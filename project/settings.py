ff83c222d276688cd619bd562b603dc6203d5cff
Frank Ocrospoma Ugaz <frankocrospomaugaz@gmail.com>
2024-02-09 21:36:22 -0500

    Corrijiendo wvds8

--- project/settings.py
+++ project/settings.py
@@ -112,7 +112,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'BDFUT',
         'USER': 'postgres',
-        'PASSWORD': 'frank2003',
+        'PASSWORD': '*CAABbgaAC*AfceGFABAeAE*d-*55C24',
         'PGHOST': 'roundhouse.proxy.rlwy.net',  # si tienes otra dirección host debes remplazar esta
         'PGPORT': '55021',  # si lo dejas vacío tomará el puerto por default
     }
